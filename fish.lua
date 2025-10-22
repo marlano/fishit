6c46253520e00e0b0e3cc9a95a5fb131eeea2b0c
MARLANO <107799912+marlano@users.noreply.github.com>
2025-10-22 20:00:10 +0700

    Update fish.lua

--- fish.lua
+++ fish.lua
@@ -1,13 +1,8 @@
 -- Fish It script v1.0
--- Author: Marllano
-
 print("[Fish It] Script aktif di game:", game.Name)
 
 local player = game.Players.LocalPlayer
-local function autoFish()
-	while task.wait(2) do
-		print("[Fish It] Memancing otomatis untuk:", player.Name)
-	end
-end
 
-task.spawn(autoFish)
+while task.wait(2) do
+	print("[Fish It] Memancing otomatis untuk:", player.Name)
+end
