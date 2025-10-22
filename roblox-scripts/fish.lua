ac58c19b008dcff46a5952dfaab216a786b7c58d
MARLANO <107799912+marlano@users.noreply.github.com>
2025-10-22 20:23:12 +0700

    Update fish.lua

--- roblox-scripts/fish.lua
+++ roblox-scripts/fish.lua
@@ -1,4 +1,12 @@
 -- Fish It script v1.0
+
+print("[Fish.lua] Script berhasil dijalankan!")
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "Fish.lua",
+    Text = "Script berhasil dijalankan!",
+    Duration = 5
+})
+
 print("[Fish It] Script aktif di game:", game.Name)
 
 local player = game.Players.LocalPlayer
