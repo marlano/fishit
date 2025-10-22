7c62a7d969d3289b563257d66180af95d9ac8f22
MARLANO <107799912+marlano@users.noreply.github.com>
2025-10-22 20:19:50 +0700

    Create fish.lua

---
+++ roblox-scripts/fish.lua
@@ -0,0 +1,8 @@
+-- Fish It script v1.0
+print("[Fish It] Script aktif di game:", game.Name)
+
+local player = game.Players.LocalPlayer
+
+while task.wait(2) do
+    print("[Fish It] Memancing otomatis untuk:", player.Name)
+end
